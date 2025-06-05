99234981dcd0f569e70282a5103f4bd69402f28e
Andy <yanandy1804@gmail.com>
2025-06-05 16:07:02 +0100

    Changed icon

--- src/main/java/phoenixoriental/restaurant/HelloApplication.java
+++ src/main/java/phoenixoriental/restaurant/HelloApplication.java
@@ -3,9 +3,11 @@ package phoenixoriental.restaurant;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
