cad136c1fe741e4e37d939815b83fac8ecd3b2ca
Andy <yanandy1804@gmail.com>
2025-06-08 15:36:36 +0100

    added fx:id

--- src/main/java/phoenixoriental/restaurant/HelloController.java
+++ src/main/java/phoenixoriental/restaurant/HelloController.java
@@ -1,7 +1,47 @@
 package phoenixoriental.restaurant;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 
 public class HelloController {
+
+    @FXML
+    private Hyperlink si_forgotPass;
+
+    @FXML
+    private Button si_loginBtn;
+
+    @FXML
+    private AnchorPane si_loginForm;
+
+    @FXML
+    private PasswordField si_password;
+
+    @FXML
+    private TextField si_username;
+
+    @FXML
+    private Button side_createBtn;
+
+    @FXML
+    private AnchorPane side_form;
+
+    @FXML
+    private TextField su_answer;
+
+    @FXML
+    private PasswordField su_password;
+
+    @FXML
+    private ComboBox<?> su_question;
+
+    @FXML
+    private Button su_signupBtn;
+
+    @FXML
+    private AnchorPane su_signupForm;
+
+    @FXML
+    private TextField su_username;
 }
