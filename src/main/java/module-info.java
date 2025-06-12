102a89658092611dd8b275dbd060466e2bf14df4
Andy <yanandy1804@gmail.com>
2025-06-12 15:03:04 +0100

    Connecting to mysql

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module phoenixoriental.restaurant {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires mysql.connector.j;
 
 
     opens phoenixoriental.restaurant to javafx.fxml;
--- src/main/java/phoenixoriental/restaurant/Database.java
+++ src/main/java/phoenixoriental/restaurant/Database.java
@@ -1,11 +1,20 @@
 package phoenixoriental.restaurant;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Database {
 
     public static Connection connectDB() {
+        String url = "jdbc:mysql://localhost:3307/restaurant";
+        String username = "root";
+        String password = "";
+
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url, username, password);
+            return connection;
         } catch(Exception e) {
             e.printStackTrace();
         }
