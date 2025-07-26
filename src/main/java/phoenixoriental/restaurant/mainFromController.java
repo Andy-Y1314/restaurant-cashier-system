d8c784d7214a531ae5adc8df820ff528dafa1973
Andy <yanandy1804@gmail.com>
2025-07-26 20:57:39 +0100

    Added fxml

---
+++ src/main/java/phoenixoriental/restaurant/mainFromController.java
@@ -0,0 +1,13 @@
+package phoenixoriental.restaurant;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class mainFromController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
