536808c8a203794175367ab1d4cbbb015fd872c2
Andy <yanandy1804@gmail.com>
2025-07-27 21:22:09 +0100

    More fxml

--- src/main/java/phoenixoriental/restaurant/mainFromController.java
+++ src/main/java/phoenixoriental/restaurant/mainFromController.java
@@ -1,11 +1,84 @@
 package phoenixoriental.restaurant;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class mainFromController implements Initializable {
+    @FXML
+    private Button customers_btn;
+
+    @FXML
+    private Button dashboard_btn;
+
+    @FXML
+    private Button inventory_addBtn;
+
+    @FXML
+    private Button inventory_btn;
+
+    @FXML
+    private Button inventory_clearBtn;
+
+    @FXML
+    private TableColumn<?, ?> inventory_col_date;
+
+    @FXML
+    private TableColumn<?, ?> inventory_col_price;
+
+    @FXML
+    private TableColumn<?, ?> inventory_col_productID;
+
+    @FXML
+    private TableColumn<?, ?> inventory_col_productName;
+
+    @FXML
+    private TableColumn<?, ?> inventory_col_status;
+
+    @FXML
+    private TableColumn<?, ?> inventory_col_stock;
+
+    @FXML
+    private TableColumn<?, ?> inventory_col_type;
+
+    @FXML
+    private Button inventory_deleteBtn;
+
+    @FXML
+    private AnchorPane inventory_form;
+
+    @FXML
+    private ImageView inventory_imageView;
+
+    @FXML
+    private Button inventory_importBtn;
+
+    @FXML
+    private TableView<?> inventory_tableView;
+
+    @FXML
+    private Button inventory_updateBtn;
+
+    @FXML
+    private Button logout_btn;
+
+    @FXML
+    private AnchorPane main_form;
+
+    @FXML
+    private Button menu_btn;
+
+    @FXML
+    private Label username;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
